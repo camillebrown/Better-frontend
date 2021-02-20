904d07adb110e59536fe51211244df28c0a939bc
“camillebrown” <camillebrown11@gmail.com>
2021-02-20 14:35:03 -0800

    updated route to get to the right URL

--- services/users.service.js
+++ services/users.service.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-const API_URL = process.env.REACT_APP_BACKEND_URL + `/api/v1/users`
+// const API_URL = process.env.REACT_APP_BACKEND_URL + `/api/v1/users`
 
 // SIGN UP
 export const register = (firstName, lastName, username, email, password) => {
-    return axios.post(API_URL + '/register', {
+    return axios.post(process.env.REACT_APP_BACKEND_URL + '/register', {
         first_name: firstName,
         last_name: lastName,
         username: username,
@@ -15,7 +15,7 @@ export const register = (firstName, lastName, username, email, password) => {
 }
 // LOG IN
 export const login = (email, password) => {
-    return axios.post(API_URL + '/login', {
+    return axios.post(process.env.REACT_APP_BACKEND_URL + '/login', {
         email, password
     },
         { withCredentials: true }
@@ -23,5 +23,5 @@ export const login = (email, password) => {
 }
 // LOG OUT
 export const logout = () => {
-    return axios.get(API_URL + '/logout', { withCredentials: true })
+    return axios.get(process.env.REACT_APP_BACKEND_URL + '/logout', { withCredentials: true })
 }
