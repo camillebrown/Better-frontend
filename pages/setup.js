cbdf8eba19f88dfcf30407ff979219501ff8eacc
Camille Brown <camillebrown@Camilles-MacBook-Pro.local>
2021-04-28 18:15:57 -0700

    testing

--- pages/setup.js
+++ pages/setup.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import Setup from '../components/Setup'
 import { Grid, GridItem, Box, Text } from "@chakra-ui/react"
 
