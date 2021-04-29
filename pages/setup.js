cd29d3daeb6bb177643e863ac725201d0d30117d
Camille Brown <camillebrown@Camilles-MacBook-Pro.local>
2021-04-28 18:09:26 -0700

    still testing

--- pages/setup.js
+++ pages/setup.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import Setup from '../components/Setup'
 import { Grid, GridItem, Box, Text } from "@chakra-ui/react"
 
@@ -26,38 +27,38 @@ const setup = () => {
             <div>
                 <img src="https://i.ibb.co/44gPmn2/Artboard-1.jpg" alt="Artboard-1" border="0" />
                 <Grid className="setup-container">
-                        <GridItem className="setup1">
-                            <Box>
-                                <Box id="hg1">
-                                    <div>
-                                        <h2 className="setup-title">Share a bit more about yourself...</h2>
-                                    </div>
-                                </Box>
-                                <Box overflow="hidden" backgroundColor="white">
-                                    <Text paddingLeft={3} pt={2} color="red" fontSize="small">* = required field</Text>
-                                    <Box className="setup-fields">
-                                        <Setup />
-                                    </Box>
+                    <GridItem className="setup1">
+                        <Box>
+                            <Box id="hg1">
+                                <div>
+                                    <h2 className="setup-title">Share a bit more about yourself...</h2>
+                                </div>
+                            </Box>
+                            <Box overflow="hidden" backgroundColor="white">
+                                <Text paddingLeft={3} pt={2} color="red" fontSize="small">* = required field</Text>
+                                <Box className="setup-fields">
+                                    <Setup />
                                 </Box>
                             </Box>
-                        </GridItem>
-                        <GridItem className="setup2">
-                            <Box>
-                                <Box overflow="hidden" backgroundColor="rgb(248, 248, 248)">
-                                    <Box paddingLeft={1} paddingRight={1}>
-                                        <Text textAlign="center" mt={5} mx={2} fontWeight="400">
-                                            Now that you have an account, <br />we need a bit more information to customize it for you!
+                        </Box>
+                    </GridItem>
+                    <GridItem className="setup2">
+                        <Box>
+                            <Box overflow="hidden" backgroundColor="rgb(248, 248, 248)">
+                                <Box paddingLeft={1} paddingRight={1}>
+                                    <Text textAlign="center" mt={5} mx={2} fontWeight="400">
+                                        Now that you have an account, <br />we need a bit more information to customize it for you!
                                         <br />
-                                            <br />
-                                            <br />
+                                        <br />
+                                        <br />
                                         Complete this process and you’re ready to start being <Text fontFamily='Boing' className="logo">
-                                                better<span className="logo-dot">.</span>
-                                            </Text>
+                                            better<span className="logo-dot">.</span>
                                         </Text>
-                                    </Box>
+                                    </Text>
                                 </Box>
                             </Box>
-                        </GridItem>
+                        </Box>
+                    </GridItem>
                 </Grid>
             </div>
         </>
