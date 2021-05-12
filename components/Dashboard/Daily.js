0b84271053cefc640984d1a62d752f1607c004b7
Camille Brown <cbrown@creditkey.com>
2021-05-12 16:55:19 -0700

    trying to fix daily workout

--- components/Dashboard/Daily.js
+++ components/Dashboard/Daily.js
@@ -10,257 +10,262 @@ dayjs.extend(localizedFormat)
 import axios from 'axios'
 
 export const Daily = (props) => {
-    // setTodayMacros([TCL, TF, TCB, TP])
-    var now = dayjs().format('LL')
-    var fNow = dayjs().format('ddd, DD MMM YYYY')
+  // setTodayMacros([TCL, TF, TCB, TP])
+  var now = dayjs().format('LL')
+  var fNow = dayjs().format('ddd, DD MMM YYYY')
 
-    const [mood, setMood] = useState()
-    const [rating, setRating] = useState()
-    const [meals, setMeals] = useState()
-    const [sleep, setSleep] = useState([])
-    const [sleeps, setSleeps] = useState([])
-    const [avgCalories, setAvgCalories] = useState("")
-    const [numWork, setNumWork] = useState("")
-    const [workouts, setWorkouts] = useState([])
+  const [mood, setMood] = useState()
+  const [rating, setRating] = useState()
+  const [meals, setMeals] = useState()
+  const [sleep, setSleep] = useState([])
+  const [sleeps, setSleeps] = useState([])
+  const [avgCalories, setAvgCalories] = useState("")
+  const [numWork, setNumWork] = useState("")
+  const [workouts, setWorkouts] = useState([])
 
-    const getTodayStats = () => {
-        axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/moods/`,
-            { withCredentials: true }
-        )
-            .then((res) => {
-                let ratings = res.data.ratings
-                if(!res.data.data){
-                    setMood(null)
-                }
-                res.data.data.forEach((mood, i )=> {
-                    let date = mood.date.substring(0, 16)
-                    let nowFormat = dayjs().format('ddd, DD MMM YYYY')
-                    if (date === nowFormat) {
-                        setMood(mood.rating+1)
-                        setRating(ratings[i])
-                    } else {
-                        setMood(null)
-                    }
-                });
-            })
-            .catch(err => {
-                console.log(err)
-            })
-        axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/meals/`,
-            { withCredentials: true }
-        )
-            .then((res) => {
-                res.data.data.forEach(meal => {
-                    let date = (meal.created_at).substring(0,16)
-                    if (date === fNow) {
-                        let meals = []
-                        meals.push(meal)
-                        setMeals(meals)
-                    } else {
-                        setMeals(null)
-                    }
-                });
-            })
-            .catch(err => {
-                console.log(err)
-            })
-        axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/sleeps/`,
-            { withCredentials: true })
-            .then((res) => {
-                setSleeps(res.data)
-                res.data.forEach(sleep => {
-                    let date = dayjs(sleep.date).format('LL')
-                    if (date === now) {
-                        const time1 = dayjs().hour(sleep.end_time.substring(0, 2))
-                        const time2 = dayjs().hour(sleep.start_time.substring(0, 2))
-                        let hours = time1.diff(time2, 'hour')
-                        let sleepTime = hours + 24
-                        setSleep(sleepTime)
-                    } else {
-                        setSleep(null)
-                    }
-                });
-            })
-            .catch(err => {
-                console.log(err)
-            })
-        axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/workouts/`,
-            { withCredentials: true }
-        )
-            .then((res) => {
-                let array = res.data.data
-                var total = 0;
-                for (var i = 0; i < res.data.data.length; i++) {
-                    total += res.data.data[i].calories;
-                }
-                var avg = total / res.data.data.length;
-                setAvgCalories(avg)
-                array.forEach(workout => {
-                    let date = workout.created_at.substring(0,16)
-                    if (date === fNow) {
-                        let workouts = []
-                        workouts.push(workout)
-                        setWorkouts(workouts)
-                        setNumWork(workouts.length)
-                    } else {
-                        setWorkouts(null)
-                    }
-
-                });
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }
-
-    useEffect(() => {
-        getTodayStats()
-    }, [])
-
-
-    return (
-        <Center>
-            <Grid className="daily-container ">
-                <GridItem className="dc1">
-                    <Box>
-                        <Box id="dc2">
-                            <div className="dc-header">
-                                <h2 className="dc-title">{now}</h2>
-                            </div>
-                        </Box>
-                    </Box>
-                </GridItem>
-                <GridItem className="dc2">
-                    <HStack px={4} py={2}>
-                        <Box>
-                            <FontAwesomeIcon
-                                className="icons"
-                                id="run-icon"
-                                icon={faRunning} />
-                        </Box>
-                        <Box px={4}>
-                            {!workouts ? (
-                                <Box>
-                                    <Text className="daily-text">
-                                        You haven’t logged any workouts today
-                                    </Text>
-                                    <Text className="daily-info">
-                                        Add your workout activity to your fitness tracker to see how it’s impacting your goals
-                                    </Text>
-                                </Box>
-                            ) : (
-                                    <Box>
-                                        <Text className="daily-text">
-                                            You've logged {numWork} workout(s) today!
-                                    </Text>
-                                        <Text className="daily-info">
-                                            Throughout all of your workouts today, you've burned an average of {avgCalories} with each workout. Great job!
-                                    </Text>
-                                    </Box>
-                                )}
-                        </Box>
-                    </HStack>
-                </GridItem>
-                <GridItem className="dc3">
-                    <HStack px={4} py={2}>
-                        <Box>
-                            <FontAwesomeIcon
-                                className="icons"
-                                id="bed-icon"
-                                icon={faBed} />
-                        </Box>
-                        <Box px={4}>
-                            {!sleeps[0] ? (
-                                <Box>
-                                    <Text className="daily-text">
-                                        You haven’t added a sleep log today
-                                    </Text>
-                                    <Text className="daily-info">
-                                        Add a sleep log to your sleep tracker to see how it’s impacting your goals
-                                    </Text>
-                                </Box>
-                            ) : (
-                                    <Box>
-                                        <Text className="daily-text">
-                                            You logged {sleep} hours of sleep from last night!
-                                        </Text>
-                                        <Text className="daily-info">
-                                            Does this line up with your sleep goals?
-                                        </Text>
-                                    </Box>
-                                )}
-                        </Box>
-                    </HStack>
-                </GridItem>
-                <GridItem className="dc4">
-                    <HStack px={4} py={2}>
-                        <Box>
-                            <FontAwesomeIcon
-                                className="icons"
-                                icon={faHamburger} />
-                        </Box>
-                        <Box px={4}>
-                            {!meals || !props.todayMacros ? (
-                                <Box>
-                                    <Text className="daily-text">
-                                        You haven’t logged any meals today
-                                    </Text>
-                                    <Text className="daily-info">
-                                        Let's get some added to your diet tracker to see how it’s impacting your goals
-                            </Text>
-                                </Box>
-                            ) : (
-                                    <Box>
-                                        <Text className="daily-text">
-                                            You've consumed {props.todayMacros[0]} calories today!
-                                        </Text>
-                                        <Text className="daily-info">
-                                            Here's your macrnutrient breakdown:<br />{props.todayMacros[3]}g Protein / {props.todayMacros[1]}g Fat / {props.todayMacros[2]}g Carbohydrates
-                                        </Text>
-                                    </Box>
-                                )}
-                        </Box>
-                    </HStack>
-                </GridItem>
-                <GridItem className="dc5">
-                    <HStack px={4} py={2}>
-                        <Box>
-                            <FontAwesomeIcon
-                                className="icons"
-                                icon={faLaughWink} />
-                        </Box>
-                        <Box px={4}>
-                            {!mood ? (
-                                <Box>
-                                    <Text className="daily-text">
-                                        You haven’t added a mood log today
-                                </Text>
-                                    <Text className="daily-info">
-                                        Add a mood log to your mood tracker to see how it’s impacting your goals
-                                </Text>
-                                </Box>
-                            ) : (
-                                    <Box>
-                                        <Text className="daily-text">
-                                            This is your mood today:<br /> "{rating}""
-                                        </Text>
-                                        <Text className="daily-info">
-                                            You gave today a {mood}/5. Could it be better?
-                                        </Text>
-                                    </Box>
-                                )}
-                        </Box>
-                    </HStack>
-                </GridItem>
-                <GridItem className="dc6">
-                    <Center>
-                        <Divider />
-                    </Center>
-                </GridItem>
-
-            </Grid >
-        </Center >
+  const getTodayStats = () => {
+    axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/moods/`,
+      { withCredentials: true }
     )
+      .then((res) => {
+        let ratings = res.data.ratings
+        if (!res.data.data) {
+          setMood(null)
+        }
+        res.data.data.forEach((mood, i) => {
+          let date = mood.date.substring(0, 16)
+          let nowFormat = dayjs().format('ddd, DD MMM YYYY')
+          if (date === nowFormat) {
+            setMood(mood.rating + 1)
+            setRating(ratings[i])
+          } else {
+            setMood(null)
+          }
+        });
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/meals/`,
+      { withCredentials: true }
+    )
+      .then((res) => {
+        res.data.data.forEach(meal => {
+          let date = (meal.created_at).substring(0, 16)
+          if (date === fNow) {
+            let meals = []
+            meals.push(meal)
+            setMeals(meals)
+          } else {
+            setMeals(null)
+          }
+        });
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/sleeps/`,
+      { withCredentials: true }
+    )
+      .then((res) => {
+        setSleeps(res.data)
+        res.data.forEach(sleep => {
+          let date = dayjs(sleep.date).format('LL')
+          if (date === now) {
+            const time1 = dayjs().hour(sleep.end_time.substring(0, 2))
+            const time2 = dayjs().hour(sleep.start_time.substring(0, 2))
+            let hours = time1.diff(time2, 'hour')
+            let sleepTime = hours + 24
+            setSleep(sleepTime)
+          } else {
+            setSleep(null)
+          }
+        });
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/workouts/`,
+      { withCredentials: true }
+    )
+      .then((res) => {
+        let array = res.data.data
+        console.log(array)
+        var total = 0;
+        for (var i = 0; i < res.data.data.length; i++) {
+          total += res.data.data[i].calories;
+        }
+        console.log(total)
+        var avg = total / res.data.data.length;
+        console.log(avg)
+        setAvgCalories(avg)
+        array.forEach(workout => {
+          let date = workout.created_at.substring(0, 16)
+          if (date === fNow) {
+            let workouts = []
+            console.log(workouts)
+            workouts.push(workout)
+            setWorkouts(workouts)
+            setNumWork(workouts.length)
+          } else {
+            setWorkouts(null)
+          }
+
+        });
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  useEffect(() => {
+    getTodayStats()
+  }, [])
+
+
+  return (
+    <Center>
+      <Grid className="daily-container ">
+        <GridItem className="dc1">
+          <Box>
+            <Box id="dc2">
+              <div className="dc-header">
+                <h2 className="dc-title">{now}</h2>
+              </div>
+            </Box>
+          </Box>
+        </GridItem>
+        <GridItem className="dc2">
+          <HStack px={4} py={2}>
+            <Box>
+              <FontAwesomeIcon
+                className="icons"
+                id="run-icon"
+                icon={faRunning} />
+            </Box>
+            <Box px={4}>
+              {!workouts ? (
+                <Box>
+                  <Text className="daily-text">
+                    You haven’t logged any workouts today
+                                    </Text>
+                  <Text className="daily-info">
+                    Add your workout activity to your fitness tracker to see how it’s impacting your goals
+                                    </Text>
+                </Box>
+              ) : (
+                <Box>
+                  <Text className="daily-text">
+                    You've logged {numWork} workout(s) today!
+                                    </Text>
+                  <Text className="daily-info">
+                    Throughout all of your workouts today, you've burned an average of {avgCalories} with each workout. Great job!
+                                    </Text>
+                </Box>
+              )}
+            </Box>
+          </HStack>
+        </GridItem>
+        <GridItem className="dc3">
+          <HStack px={4} py={2}>
+            <Box>
+              <FontAwesomeIcon
+                className="icons"
+                id="bed-icon"
+                icon={faBed} />
+            </Box>
+            <Box px={4}>
+              {!sleeps[0] ? (
+                <Box>
+                  <Text className="daily-text">
+                    You haven’t added a sleep log today
+                                    </Text>
+                  <Text className="daily-info">
+                    Add a sleep log to your sleep tracker to see how it’s impacting your goals
+                                    </Text>
+                </Box>
+              ) : (
+                <Box>
+                  <Text className="daily-text">
+                    You logged {sleep} hours of sleep from last night!
+                                        </Text>
+                  <Text className="daily-info">
+                    Does this line up with your sleep goals?
+                                        </Text>
+                </Box>
+              )}
+            </Box>
+          </HStack>
+        </GridItem>
+        <GridItem className="dc4">
+          <HStack px={4} py={2}>
+            <Box>
+              <FontAwesomeIcon
+                className="icons"
+                icon={faHamburger} />
+            </Box>
+            <Box px={4}>
+              {!meals || !props.todayMacros ? (
+                <Box>
+                  <Text className="daily-text">
+                    You haven’t logged any meals today
+                                    </Text>
+                  <Text className="daily-info">
+                    Let's get some added to your diet tracker to see how it’s impacting your goals
+                            </Text>
+                </Box>
+              ) : (
+                <Box>
+                  <Text className="daily-text">
+                    You've consumed {props.todayMacros[0]} calories today!
+                                        </Text>
+                  <Text className="daily-info">
+                    Here's your macrnutrient breakdown:<br />{props.todayMacros[3]}g Protein / {props.todayMacros[1]}g Fat / {props.todayMacros[2]}g Carbohydrates
+                                        </Text>
+                </Box>
+              )}
+            </Box>
+          </HStack>
+        </GridItem>
+        <GridItem className="dc5">
+          <HStack px={4} py={2}>
+            <Box>
+              <FontAwesomeIcon
+                className="icons"
+                icon={faLaughWink} />
+            </Box>
+            <Box px={4}>
+              {!mood ? (
+                <Box>
+                  <Text className="daily-text">
+                    You haven’t added a mood log today
+                                </Text>
+                  <Text className="daily-info">
+                    Add a mood log to your mood tracker to see how it’s impacting your goals
+                                </Text>
+                </Box>
+              ) : (
+                <Box>
+                  <Text className="daily-text">
+                    This is your mood today:<br /> "{rating}""
+                                        </Text>
+                  <Text className="daily-info">
+                    You gave today a {mood}/5. Could it be better?
+                                        </Text>
+                </Box>
+              )}
+            </Box>
+          </HStack>
+        </GridItem>
+        <GridItem className="dc6">
+          <Center>
+            <Divider />
+          </Center>
+        </GridItem>
+
+      </Grid >
+    </Center >
+  )
 }
 
 export default Daily
