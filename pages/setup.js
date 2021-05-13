65915397d3c2b69acca228a697241121424b7cb9
Camille Brown <cbrown@creditkey.com>
2021-05-12 19:35:58 -0700

    removed unnecessary method

--- pages/setup.js
+++ pages/setup.js
@@ -1,13 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios';
 import Setup from '../components/Setup'
 import { Grid, GridItem, Box, Text } from "@chakra-ui/react"
 
 const setup = () => {
-
-    useEffect(() => {
-        getUserInfo()
-    }, [])
 
     return (
         <>
